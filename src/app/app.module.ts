939208f26777baa3e254e74b1bae9687565e04a4
bagool185 <ioan.adrian98@gmail.com>
2018-01-25 01:40:34 +0000

    removed tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 import { ArchiveTodosPage } from './../pages/archive-todos/archive-todos';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -21,7 +20,6 @@ import { TodoProvider } from '../providers/todo/todo';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
     ArchiveTodosPage
   ],
   imports: [
@@ -35,7 +33,6 @@ import { TodoProvider } from '../providers/todo/todo';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
     ArchiveTodosPage
   ],
   providers: [
